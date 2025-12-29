26addf19a92341be918c86d61b4319cd748271e4
Márcio Carvalho Jr <maccarvalho19@gmail.com>
2025-12-29 08:00:46 -0300

    Meu primeiro commit

---
+++ teste1_classificador_heroi.js
@@ -0,0 +1,38 @@
+
+
+let heros = [
+    { nome: "Naruto", xp: 915 },
+    { nome: "Goku", xp: 8100 },
+    { nome: "Luffy", xp: 6000 },
+    { nome: "Saitama", xp: 11000 }
+];
+
+function Classificador_nivel() {
+    for (let i = 0; i < heros.length; i++) {
+        let nome = heros[i].nome
+        let xp = heros[i].xp
+        let nivel = "";
+
+        if (xp < 1000) {
+            nivel = "Ferro";
+        } else if (xp >= 1000 && xp < 2000 ){
+            nivel = "Bronze";
+        } else if (xp >= 2001 && xp < 5000 ){
+            nivel = "Prata";
+        } else if (xp >= 5001 && xp < 7000 ){
+            nivel = "Ouro";
+        } else if (xp >= 7001 && xp < 8000 ){
+            nivel = "Platina";
+        } else if (xp >= 8001 && xp < 9000 ){
+            nivel = "Ascendente";
+        } else if (xp >= 9001 && xp < 10000 ){
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
+        }
+
+        console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+    }
+}
+
+Classificador_nivel();
